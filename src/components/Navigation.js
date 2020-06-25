2213c8cb9201df25225e82b54a202cc3088d3e80
Usama Subhani <reachusama@protonmail.com>
2020-06-25 08:42:28 +0500

    Style navigation bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import { GlobalContext } from '../context/GlobalContext'
-
+import './Navigation.css';
 import { Link } from "react-router-dom";
 
 function Navigation() {
